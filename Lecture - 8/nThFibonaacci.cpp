15fd4f2f47c2a8f9a4e16286e200c59de9fa5fa1
readwanmd <readwanmd@gmail.com>
2022-10-24 17:24:03 +0600

    hw done

--- Lecture - 8/nThFibonaacci.cpp
+++ Lecture - 8/nThFibonaacci.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 int fibonacci(int n)
 {
-  int num1 = 0;
-  int num2 = 1;
+  int num1 = 0, num2 = 1, feb = 0;
 
-  while (n--)
+  for (int i = 1; i <= n - 2; i++)
   {
+    feb = num1 + num2;
+    num1 = num2;
+    num2 = feb;
   }
+
+  return feb;
 }
 
 int main()
 {
-  int n = 5;
+  // int n = 5;
+
+  cout << fibonacci(10) << endl;
 
   return 0;
 }
